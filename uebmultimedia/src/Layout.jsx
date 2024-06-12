98c28e70a4a1df00a421f0921d017e2817c52f1f
ArdiBerdyna <ardi_881@hotmail.com>
2024-06-12 16:17:29 +0200

    add photos and videos

---
+++ uebmultimedia/src/Layout.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+import LoginForm from "./components/User/LoginForm"
+import Registration from "./components/User/Registration"
+
+function Layout() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/login' component={LoginForm} />
+          <Route exact path='/register' component={Registration} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
+  
+  export default Layout;
